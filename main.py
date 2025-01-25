6ea43d0c9d8d136128a185374cb75a5be1d0d65a
mnquackenbush <mnquackenbush@gmail.com>
2025-01-25 15:09:28 +1100

    Added sort_by_count function; Streamlined character_sorter function; Added and updated code comments

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ def main():
     print()
 
     #Print each letter's count
-    for letter in letter_counts:
-        print(f"The '{letter}' character was found {letter_counts[letter]} times")
+    for dict in letter_counts:
+        char_value = dict["char"]
+        char_count = dict["num"]
+        print(f"The '{char_value}' character was found {char_count} times")
 
     print("--- End report ---")
 
@@ -46,23 +48,40 @@ def character_counter(text):
                 character_count[character] += 1   
     return character_count       
 
+# Takes in a dictionary and transforms its pairs into tuples,
+# then sorts the tuples in descending order according to their values
+# (second index in each tuple).
+def sort_by_count(dict):
+    # Defines a list to store each new tuple in.
+    chars_to_sort = []
+    
+    # Iterates through dict argument and changes each key-value pair into
+    # a new dictionary, then appends each dictionary to the chars_to_sort list.
+    for key in dict:
+        char_count = {"char": key, "num": dict[key]}
+        chars_to_sort.append(char_count)
+    
+    #Defines key function for sorting below
+    def sort_on(dict):
+        return dict["num"]
+    
+    # Sorts chars_to_sort in descending order,
+    # according to the "num" key.
+    chars_to_sort.sort(reverse=True, key=sort_on)
+    
+    #Returns the final sorted list
+    return chars_to_sort
+
 # Takes in a dictionary (specifically character_counts)
 # and sorts each character into a respective subdictionary 
 # by character type.
 def character_sorter(characters):
-    #Initializes 3 Separate Dictionaries for Each Character Type
+    # Initializes 3 Separate Dictionaries for Each Character Type
     letters = {}
     numbers = {}
     special_chars = {}
-
-    #Initializes a list to store each character dictionary
-    character_list = [
-        letters,
-        numbers,
-        special_chars
-    ]
     
-    #Sorts characters (and their counts) into respective dictionaries
+    # Sorts characters (and their counts) into respective dictionaries
     for character in characters:
         if character.isalpha():
             if character not in letters:
@@ -73,8 +92,13 @@ def character_sorter(characters):
         else:
             if character not in special_chars:
                 special_chars[character] = characters[character]
-    
-    #Returns list of sorted dictionaries
-    return character_list
+
+    # Sorts each dictionary by character count in descending order.
+    letters = sort_by_count(letters)
+    numbers = sort_by_count(numbers)
+    special_chars = sort_by_count(special_chars)
+
+    # Returns list of sorted dictionaries
+    return letters, numbers, special_chars
 
 main()
